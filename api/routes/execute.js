7648427b29a6f95f9666760c538ee0da2ca24066
steven07a <stevenherrera70396@gmail.com>
2022-11-20 04:07:36 -0800

    cards are now dynamic

--- api/routes/execute.js
+++ api/routes/execute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { runFile, getBlogsAndBlogID } from "../controllers/execute.js";
+import { runFile, getBlogsAndBlogID, getBlog } from "../controllers/execute.js";
 
 const router = express.Router()
 
 router.post("/runFile", runFile);
 router.get("/getBlogsAndBlogID", getBlogsAndBlogID);
+router.get("/getBlog/:id", getBlog);
 
 export default router
